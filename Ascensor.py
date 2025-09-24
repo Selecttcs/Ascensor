cd360576ec2fa48bf60c144ddd28bdf71144f8ff
Diego Hernán Cerda Mercado <diego.cerda@factorit.com>
2025-09-24 10:38:51 -0300

    v224092025

--- Ascensor.py
+++ Ascensor.py
@@ -16,7 +16,8 @@ V_PISO_ACTUAL_ASCENSOR_DER  = 0
 
 V_PRIMER_PISO = 1
 V_ULTIMO_PISO = 15
-
+V_DISTANCIA_PERSONA_IZQ = 0
+V_DISTANCIA_PERSONA_DER = 0
 
 ##Variable para el bucle...
 SW = True
@@ -30,10 +31,30 @@ while SW:
     V_PISO_ACTUAL_ASCENSOR_IZQ = random.randint(V_PRIMER_PISO, V_ULTIMO_PISO)
     V_PISO_ACTUAL_ASCENSOR_DER = random.randint(V_PRIMER_PISO, V_ULTIMO_PISO)
 
+    V_DISTANCIA_PERSONA_IZQ = V_PISO_ACTUAL_PERSONA - V_PISO_ACTUAL_ASCENSOR_IZQ
+    V_DISTANCIA_PERSONA_DER = V_PISO_ACTUAL_PERSONA - V_PISO_ACTUAL_ASCENSOR_DER
+    V_DISTANCIA_TOTAL = V_DISTANCIA_PERSONA_IZQ - V_DISTANCIA_PERSONA_DER
+
     print(f'Usted se encuentra en el piso {V_PISO_ACTUAL_PERSONA}')
     print(f'El ascensor izquierdo actualmente se encuentra en el piso {V_PISO_ACTUAL_ASCENSOR_IZQ}\n')
-    print(f'El ascensor derecho actualmente se encuentra ene l piso {V_PISO_ACTUAL_ASCENSOR_DER}\n')
+    print(f'El ascensor derecho actualmente se encuentra en el piso {V_PISO_ACTUAL_ASCENSOR_DER}\n')
     op = int(input('¿Sube o baja?\n1)Sube\n2)Baja\nR:'))
+    
+    ##Condicionales para saber si subo o baja el ascensor de la izquierda o la derecha
+    if V_DISTANCIA_PERSONA_IZQ < V_DISTANCIA_PERSONA_DER:
+        print(f'Yendo ascensor de la izquierda piso {V_PISO_ACTUAL_ASCENSOR_IZQ} hacia la persona que se encuentra')
+
+        print(f'en el piso {V_PISO_ACTUAL_PERSONA}')
+        SW = False
+
+    elif V_DISTANCIA_PERSONA_DER < V_DISTANCIA_PERSONA_IZQ:
+        print(f'Yendo ascensor de la derecha piso {V_PISO_ACTUAL_ASCENSOR_DER} hacia la persona que se encuentra')
+
+        print(f'en el piso {V_PISO_ACTUAL_PERSONA}')   
+        SW = False
+        
+
+
 
     if op == 1:
         print('Subiendo...')
