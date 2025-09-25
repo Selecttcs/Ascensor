fe1c376f01d71a18fb47a5427c709112b0091197
Diego Hernán Cerda Mercado <diego.cerda@factorit.com>
2025-09-24 23:44:48 -0300

    V224092025

--- Ascensor.py
+++ Ascensor.py
@@ -26,50 +26,79 @@ SW = True
 while SW:
 
     os.system('clear')
-    print('Bienvenido al ascensor Alto Colon')
+    print('*************************************')
+    print('*                                   *')
+    print('* Bienvenido al ascensor Alto Colon *')
+    print('*                                   *')
+    print('*************************************\n')
     V_PISO_ACTUAL_PERSONA = random.randint(V_PRIMER_PISO, V_ULTIMO_PISO)
     V_PISO_ACTUAL_ASCENSOR_IZQ = random.randint(V_PRIMER_PISO, V_ULTIMO_PISO)
     V_PISO_ACTUAL_ASCENSOR_DER = random.randint(V_PRIMER_PISO, V_ULTIMO_PISO)
 
-    V_DISTANCIA_PERSONA_IZQ = V_PISO_ACTUAL_PERSONA - V_PISO_ACTUAL_ASCENSOR_IZQ
-    V_DISTANCIA_PERSONA_DER = V_PISO_ACTUAL_PERSONA - V_PISO_ACTUAL_ASCENSOR_DER
-    V_DISTANCIA_TOTAL = V_DISTANCIA_PERSONA_IZQ - V_DISTANCIA_PERSONA_DER
-
-    print(f'Usted se encuentra en el piso {V_PISO_ACTUAL_PERSONA}')
-    print(f'El ascensor izquierdo actualmente se encuentra en el piso {V_PISO_ACTUAL_ASCENSOR_IZQ}\n')
-    print(f'El ascensor derecho actualmente se encuentra en el piso {V_PISO_ACTUAL_ASCENSOR_DER}\n')
-    op = int(input('¿Sube o baja?\n1)Sube\n2)Baja\nR:'))
+    V_DISTANCIA_PERSONA_IZQ = V_PISO_ACTUAL_ASCENSOR_IZQ - V_PISO_ACTUAL_PERSONA
+    V_DISTANCIA_PERSONA_DER = V_PISO_ACTUAL_ASCENSOR_DER - V_PISO_ACTUAL_PERSONA
+    #Validacion si el numero es negativo cambiar a positivo el signo
+    if V_DISTANCIA_PERSONA_IZQ < 0:
+        V_DISTANCIA_PERSONA_IZQ = 0
+        V_DISTANCIA_PERSONA_IZQ = V_PISO_ACTUAL_PERSONA - V_PISO_ACTUAL_ASCENSOR_IZQ
     
-    ##Condicionales para saber si subo o baja el ascensor de la izquierda o la derecha
-    if V_DISTANCIA_PERSONA_IZQ < V_DISTANCIA_PERSONA_DER:
-        print(f'Yendo ascensor de la izquierda piso {V_PISO_ACTUAL_ASCENSOR_IZQ} hacia la persona que se encuentra')
+    if V_DISTANCIA_PERSONA_DER < 0:
+        V_DISTANCIA_PERSONA_DER = 0
+        V_DISTANCIA_PERSONA_DER = V_PISO_ACTUAL_PERSONA - V_PISO_ACTUAL_ASCENSOR_DER
 
-        print(f'en el piso {V_PISO_ACTUAL_PERSONA}')
-        SW = False
+    V_DISTANCIA_TOTAL = V_DISTANCIA_PERSONA_IZQ - V_DISTANCIA_PERSONA_DER
+    
+    print('*************************************')
+    print('*                                   *')
+    print(f'* Usted se encuentra en el piso {V_PISO_ACTUAL_PERSONA}   *')
+    print('*                                   *')
+    print('*************************************\n')
+    
+    print('*****************************************************************')
+    print('*                                                                *')
+    print(f'* El ascensor IZQUIERDO actualmente se encuentra en el piso {V_PISO_ACTUAL_ASCENSOR_IZQ} *\n')
+    print(f'* El ascensor DERECHO actualmente se encuentra en el piso {V_PISO_ACTUAL_ASCENSOR_DER} *')
+    print('*                                                              *')
+    print(f'****************************************************************\n')
+    
 
-    elif V_DISTANCIA_PERSONA_DER < V_DISTANCIA_PERSONA_IZQ:
-        print(f'Yendo ascensor de la derecha piso {V_PISO_ACTUAL_ASCENSOR_DER} hacia la persona que se encuentra')
+    try:    
+        op = int(input('¿Desea presionar el botón? \n1)Presionar 2)No presionar\nR:'))
+        if op == 1:
+        ##Condicionales para saber si subo o baja el ascensor de la izquierda o la derecha
+            if V_DISTANCIA_PERSONA_IZQ <= V_DISTANCIA_PERSONA_DER:
+                print(f'\n\nYendo ascensor de la izquierda piso {V_PISO_ACTUAL_ASCENSOR_IZQ} hacia la persona que se encuentra')
 
-        print(f'en el piso {V_PISO_ACTUAL_PERSONA}')   
-        SW = False
-        
+                print(f'en el piso {V_PISO_ACTUAL_PERSONA}.')
 
+                print('Explicación:')
+                print(f'Porque la distancia entra la persona y el ascensor izquierdo ({V_DISTANCIA_PERSONA_IZQ}) es inferior a la')
+                print(f'distancia ascensor derecho ({V_DISTANCIA_PERSONA_DER})')
+                SW = False
 
+            elif V_DISTANCIA_PERSONA_DER <= V_DISTANCIA_PERSONA_IZQ:
+                print(f'\n\nYendo ascensor de la derecha piso {V_PISO_ACTUAL_ASCENSOR_DER} hacia la persona que se encuentra')
+            
+                print(f'en el piso {V_PISO_ACTUAL_PERSONA}')
 
-    if op == 1:
-        print('Subiendo...')
-        time.sleep(3)
-        SW = False
-    elif op == 2:
-        print('Bajando...')
-        time.sleep(3)
-        SW = False
-    else:
+                print('\n\n\nExplicación:')
+                print(f'Porque la distancia entra la persona y el ascensor derecho ({V_DISTANCIA_PERSONA_DER}) es inferior a la')
+                print(f'distancia ascensor izquierdo ({V_DISTANCIA_PERSONA_IZQ})')
+                SW = False
+        elif op == 2: 
+            print('Hasta luego!')
+            time.sleep(3)
+            SW = False
+
+        else:
+            os.system('clear')
+            print('Escoga los números indicados...')
+            time.sleep(3)
+    except:
         os.system('clear')
-        print('Escoga los números indicados...')
+        print('Ocurrió un error, probablemente pusiste una letra donde debía ir un número...')
         time.sleep(3)
-             
-
+        
 
 
 
